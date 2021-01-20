18ad3e4c36a9b663f2866f895066211dd0b48fa2
F-Gholami <fereshteh_gholami@outlook.com>
2021-01-20 10:13:08 +0100

    adding all files

---
+++ quoteGenerator.js
@@ -0,0 +1,305 @@
+let quotes = [{
+    quote: 'There is no blue without yellow and without orange.',
+    author: 'Vincent Van Gogh',
+    category: 'artist',
+}, {
+    quote: 'Poetry surrounds us everywhere, but putting it on paper is, alas, not so easy as looking at it.',
+    author: 'Vincent Van Gogh',
+    category: 'artist',
+}, {
+    quote: 'The fishermen know that the sea is dangerous and the storm terrible, but they have never found these dangers sufficient reason for remaining ashore.',
+    author: 'Vincent Van Gogh',
+    category: 'artist',
+}, {
+    quote: 'There may be a great fire in our hearts, yet no one ever comes to warm himself at it, and the passers-by see only a wisp of smoke.',
+    author: 'Vincent Van Gogh',
+    category: 'artist',
+}, {
+    quote: 'I dream of painting and then I paint my dream.',
+    author: 'Vincent Van Gogh',
+    category: 'artist',
+}, {
+    quote: 'Drawing is the honesty of the art. There is no possibility of cheating. It is either good or bad.',
+    author: 'Salvador Dali',
+    category: 'artist',
+}, {
+    quote: 'Painting is an infinitely minute part of my personality.',
+    author: 'Salvador Dali',
+    category: 'artist',
+}, {
+    quote: 'Surrealism is destructive, but it destroys only what it considers to be shackles limiting our vision.',
+    author: 'Salvador Dali',
+    category: 'artist',
+}, {
+    quote: 'There are some days when I think I am going to die from an overdose of satisfaction.',
+    author: 'Salvador Dali',
+    category: 'artist',
+}, {
+    quote: 'There is only one difference between a madman and me. The madman thinks he is sane. I know I am mad.',
+    author: 'Salvador Dali',
+    category: 'artist',
+}, {
+    quote: 'Each morning when I awake, I experience again a supreme pleasure - that of being Salvador Dali.',
+    author: 'Salvador Dali',
+    category: 'artist',
+}, {
+    quote: 'I do not do drugs. I am drugs.',
+    author: 'Salvador Dali',
+    category: 'artist',
+}, {
+    quote: 'Being good in business is the most fascinating kind of art. Making money is art and working is art and good business is the best art.',
+    author: 'Andy Warhole',
+    category: 'artist',
+}, {
+    quote: 'I love Los Angeles, and I love Hollywood. They are beautiful. Everybody is plastic, but I love plastic. I want to be plastic.',
+    author: 'Andy Warhole',
+    category: 'artist',
+}, {
+    quote: 'The most exciting attractions are between two opposites that never meet.',
+    author: 'Andy Warhole',
+    category: 'artist',
+}, {
+    quote: 'When you think about it, department stores are kind of like museums.',
+    author: 'Andy Warhole',
+    category: 'artist',
+}, {
+    quote: 'Everyone needs a fantasy.',
+    author: 'Andy Warhole',
+    category: 'artist',
+}, {
+    quote: 'I have always wanted to work for Walt Disney. That is what I thought I was going to do when I grew up.',
+    author: 'Keith Haring',
+    category: 'artist',
+}, {
+    quote: 'Nothing is important... so everything is important.',
+    author: 'Keith Haring',
+    category: 'artist',
+}, {
+    quote: 'Art is nothing if you don ot reach every segment of the people.',
+    author: 'Keith Haring',
+    category: 'artist',
+}, {
+    quote: 'I have been enlightened. I have fallen into poetry and it has swallowed me up.',
+    author: 'Keith Haring',
+    category: 'artist',
+}, {
+    quote: 'Art is for everybody.',
+    author: 'Keith Haring',
+    category: 'artist',
+}, {
+    quote: 'I saw the angel in the marble and carved until I set him free.',
+    author: 'Michelangelo',
+    category: 'artist',
+}, {
+    quote: 'My soul can find no staircase to Heaven unless it be through Earth is loveliness.',
+    author: 'Michelangelo',
+    category: 'artist',
+}, {
+    quote: 'I am a poor man and of little worth, who is laboring in that art that God has given me in order to extend my life as long as possible.',
+    author: 'Michelangelo',
+    category: 'artist',
+}, {
+    quote: 'Carving is easy, you just go down to the skin and stop',
+    author: 'Michelangelo',
+    category: 'artist',
+}, {
+    quote: 'Every beauty which is seen here by persons of perception resembles more than anything else that celestial source from which we all are come.',
+    author: 'Michelangelo',
+    category: 'artist',
+}, {
+    quote: 'Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning.',
+    author: 'Albert Einstein',
+    category: 'scientist',
+}, {
+    quote: 'It is the supreme art of the teacher to awaken joy in creative expression and knowledge.',
+    author: 'Albert Einstein',
+    category: 'scientist',
+}, {
+    quote: 'To raise new questions, new possibilities, to regard old problems from a new angle, requires creative imagination and marks real advance in science.',
+    author: 'Albert Einstein',
+    category: 'scientist',
+}, {
+    quote: 'Most people say that it is the intellect which makes a great scientist. They are wrong: it is character.',
+    author: 'Albert Einstein',
+    category: 'scientist',
+}, {
+    quote: 'When I want to understand what is happening today or try to decide what will happen tomorrow, I look back.',
+    author: 'Omar Khayyam',
+    category: 'scientist',
+}, {
+    quote: 'Drink! for you know not whence you came nor why: drink! for you know not why you go, nor where.',
+    author: 'Omar Khayyam',
+    category: 'scientist',
+}, {
+    quote: 'You know, my friends, with what a brave carouse I made a Second Marriage in my house; favored old barren reason from my bed, and took the daughter of the vine to spouse.',
+    author: 'Omar Khayyam',
+    category: 'scientist',
+}, {
+    quote: 'Therefore in medicine we ought to know the causes of sickness and health.',
+    author: 'Avicenna',
+    category: 'scientist',
+}, {
+    quote: 'The knowledge of anything, since all things have causes, is not acquired or complete unless it is known by its causes.',
+    author: 'Avicenna',
+    category: 'scientist',
+}, {
+    quote: 'That whose existence is necessary must necessarily be one essence.',
+    author: 'Avicenna',
+    category: 'scientist',
+}, {
+    quote: 'As to the mental essence, we find it in infants devoid of every mental form.',
+    author: 'Avicenna',
+    category: 'scientist',
+}, {
+    quote: 'Truth is ever to be found in simplicity, and not in the multiplicity and confusion of things.',
+    author: 'Isaac Newton',
+    category: 'scientist',
+}, {
+    quote: 'My powers are ordinary. Only my application brings me success.',
+    author: 'Isaac Newton',
+    category: 'scientist',
+}, {
+    quote: 'To every action there is always opposed an equal reaction.',
+    author: 'Isaac Newton',
+    category: 'scientist',
+}, {
+    quote: 'Atheism is so senseless. When I look at the solar system, I see the earth at the right distance from the sun to receive the proper amounts of heat and light. This did not happen by chance.',
+    author: 'Isaac Newton',
+    category: 'scientist',
+}, {
+    quote: 'Tact is the art of making a point without making an enemy.',
+    author: 'Isaac Newton',
+    category: 'scientist',
+}, {
+    quote: 'In much of society, research means to investigate something you do not know or understand.',
+    author: 'Neil Armstrong',
+    category: 'scientist',
+}, {
+    quote: 'I believe every human has a finite number of heartbeats. I do not intend to waste any of mine.',
+    author: 'Neil Armstrong',
+    category: 'scientist',
+}, {
+    quote: 'Geologists have a saying - rocks remember.',
+    author: 'Neil Armstrong',
+    category: 'scientist',
+}, {
+    quote: 'NASA has been one of the most successful public investments in motivating students to do well and achieve all they can achieve. It is sad that we are turning the programme in a direction where it will reduce the amount of motivation and stimulation it provides to young people.',
+    author: 'Neil Armstrong',
+    category: 'scientist',
+}, {
+    quote: 'Pilots take no special joy in walking. Pilots like flying.',
+    author: 'Neil Armstrong',
+    category: 'scientist',
+},
+//After generalization of the randomize function, it can get any new category just by giving the new data to it....
+{
+    quote: 'To be or not to be!',
+    author: 'William Shakespeare',
+    category: 'poet',
+}
+]
+
+/***********
+ * Variables
+ ***********/
+//Get the modal element
+let quoteModal = document.getElementById('quoteModal')
+//Get the modam button
+let getQuoteBtn = document.getElementById('getQuoteBtn')
+//Get the close button
+let closeBtn = document.getElementsByClassName('closeBtn')[0]
+//Get modal paragraph to put a new quote in it
+let quoteText = document.getElementById('quoteText')
+//Get Artists Button
+let artistsBtn = document.getElementById('artistsBtn')
+//Get Scientists Button
+let scientistsBtn = document.getElementById('scientistsBtn')
+//Get number of quotes
+let inputNumber = document.getElementById('inputNumber')
+//Keep track of which button has been clicked
+var buttonClicked = ''
+//Declare a var and set attribute to it to change the bg
+var modalClass = ''
+
+/********************
+ * Randomize Function
+ ********************/
+let randomize = (cat) => {
+    let quoteArray = quotes.filter((category) => { return category.category === cat })
+
+    if (quoteArray.length !== 0) {
+        let quoteObject = quoteArray[Math.floor(Math.random() * quoteArray.length)]
+        return quoteObject
+    } else {
+        return { quote: '', author: '', category: '' }
+    }
+}
+
+/***********************************************
+ * Focus on the form to get the number of Quotes
+ ***********************************************/
+focusOnNumberOfQuotes = () => {
+    inputNumber.focus({ preventScroll: true })
+    inputNumber.setAttribute('class', 'offset-xs-3 col-xs-6 offset-sm-3 col-sm-6 focus')
+}
+blurNumberOfQuotes = () => {
+    inputNumber.blur()
+    inputNumber.setAttribute('class', 'offset-xs-3 col-xs-6 offset-sm-3 col-sm-6')
+}
+
+/*********************************************
+ * Get value from HTML... value of the buttons
+ *********************************************/
+buttonClickHandler = (event) => {
+    buttonClicked = event.target.value
+    modalClass = 'modal-content modal-' + event.target.value
+    focusOnNumberOfQuotes()
+}
+artistsBtn.addEventListener('click', buttonClickHandler)
+scientistsBtn.addEventListener('click', buttonClickHandler)
+
+/*************************************************************
+ * Get Quote Function... Open the modal with the quotes inside
+ *************************************************************/
+openModal = () => {
+    //Get value of number of quotes
+    let inputValue = document.getElementById('inputNumber').value
+
+    var quote = ''
+    if (buttonClicked) {
+        for (i = 1; i <= inputValue; i++) {
+            let quoteObject = randomize(buttonClicked)
+            quote += `<br> "<strong>${quoteObject.quote}</strong>" <br> <footer><cite><small><i>_${quoteObject.author}_</i></small></cite></footer><br>`
+        }
+        quoteText.innerHTML = quote
+        document.getElementById('modal-content').setAttribute('class', modalClass)
+        quoteModal.style.display = 'block';
+    } else {
+        alert('Please select a category from which you want the quote!')
+    }
+
+}
+getQuoteBtn.addEventListener('click', openModal)
+
+
+/****************************
+ *Function to close the modal
+ ****************************/
+closeModal = () => {
+    quoteModal.style.display = 'none';
+    blurNumberOfQuotes()
+}
+closeBtn.addEventListener('click', closeModal)
+
+/******************************************
+ * Function to close modal if click outside
+ ******************************************/
+clickOutside = (e) => {
+    //The target has to be equal whit modal
+    //we pass an event parameter
+    if (e.target == quoteModal) {
+        quoteModal.style.display = 'none'
+        blurNumberOfQuotes()
+    }
+}
+window.addEventListener('click', clickOutside)
